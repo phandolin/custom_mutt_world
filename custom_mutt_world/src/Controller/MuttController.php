46448d233877ec834eb55588ae5c02c7a928810c
phandolin <phandolin@gmail.com>
2019-04-10 12:56:34 -0700

    Twig debugging.

--- custom_mutt_world/src/Controller/MuttController.php
+++ custom_mutt_world/src/Controller/MuttController.php
@@ -31,7 +31,7 @@ class MuttController extends ControllerBase {
           'view' => $mutts_view
         ];
         $build['#theme'] = 'custom_mutt_world';
-        ksm($build);
+        ksm($mutts_view);
         return $build;
     } else {
     return [
