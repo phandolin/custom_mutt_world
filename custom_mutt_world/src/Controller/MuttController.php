9cd588965965ca4f0b2c07f88efb997e810f65bb
phandolin <phandolin@gmail.com>
2019-04-10 11:41:54 -0700

    Updated view controller variable

--- custom_mutt_world/src/Controller/MuttController.php
+++ custom_mutt_world/src/Controller/MuttController.php
@@ -16,17 +16,17 @@ class MuttController extends ControllerBase {
    *   Return Hello string.
    */
   public function mutt() {
-    $account = \Drupal::currentUser();
-    if(!$account->hasPermission('access mutts')) {
-      return [
-        '#markup' => t('You are no authorized to view this page')
-      ];
-    }
-
-    return [
-      '#type' => 'markup',
-      '#markup' => $this->t('Let me hear it for the mutts in the back!')
-    ];
+    // $account = \Drupal::currentUser();
+    // if(!$account->hasPermission('access mutts')) {
+    //   return [
+    //     '#markup' => t('You are no authorized to view this page')
+    //   ];
+    // }
+    //
+    // return [
+    //   '#type' => 'markup',
+    //   '#markup' => $this->t('Let me hear it for the mutts in the back!')
+    // ];
 
   $suggestion_view = views_embed_view('mutts', 'mutts');
 
